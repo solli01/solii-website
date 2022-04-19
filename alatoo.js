83f4c41327cae8bbf118422cfc761c2f72c4a634
solli01 <92916622+solli01@users.noreply.github.com>
2022-04-19 16:59:24 +0600

    Create alatoo.js

---
+++ alatoo.js
@@ -0,0 +1,10 @@
+function display(form){
+    if(form.username.value=="syrga"){
+        if(form.password.value=="123456"){
+            location="asd.php"
+        } else{
+            alert("Invalid password")
+        }
+    }  else { alert("Inavalid Username")
+    }
+}
